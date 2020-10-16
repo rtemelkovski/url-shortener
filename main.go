87c0e7e0cc1610224b3b9141193d0ea2c9660d4d
Roberto Temelkovski <temelkr@mcmaster.ca>
2020-10-16 15:27:17 -0400

    finished and commented

--- main.go
+++ main.go
@@ -4,79 +4,85 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/rtemelkovski/url-shortener/urlshort"
 	"gopkg.in/yaml.v2"
+
+	"github.com/rtemelkovski/url-shortener/urlshort"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-}
+/*
+decoderAdapter - Function type for converting a json or yaml specific Decoder to our own custom Decoder.
+By adapting these types to our own interface, we can generalize all the Decoding work instead of writing
+a seperate functions for both yaml and json. Seeing as all the Decoder types take an io.Reader as input,
+we follow this trend.
 
-func getYAMLDecoder(yamlPath *string) (*yaml.Decoder, error) {
-	reader, err := os.Open(*yamlPath)
+Example:
+func(reader io.Reader) urlshort.Decoder {
+	decoder := yaml.NewDecoder(reader)
+	return decoder
+}
+*/
+type decoderAdapter func(io.Reader) urlshort.Decoder
+
+/*
+Extends the paths of a given redirector using the configs in the file at the given path. decoderAdapter
+allows us to read in any type of file and convert it to our own custom Decoder. This way, parsing from
+new types of files can be done easily by just creating a new decoderAdapter for that file type.
+*/
+func extendPaths(redirector http.HandlerFunc, path *string, adaptDecoder decoderAdapter) http.HandlerFunc {
+	// attempt to open the file. Print an error message and skip this file's registration if there is an error
+	reader, err := os.Open(*path)
 	if err != nil {
-		return nil, err
+		fmt.Printf("Cannot open file %s, skipping registration for this file\n", *path)
+		return redirector
 	}
 
-	return yaml.NewDecoder(reader), nil
-}
-
-func getJSONDecoder(jsonPath *string) (*json.Decoder, error) {
-	reader, err := os.Open(*jsonPath)
+	// attempt to decode the file and append the paths. Print an error message and skip this file's registration if there is an error
+	newPaths, err := urlshort.DecodeToMap(adaptDecoder(reader))
 	if err != nil {
-		return nil, err
-	}
-
-	return json.NewDecoder(reader), nil
-}
-
-func extendPaths(redirector http.HandlerFunc, decoder urlshort.Decoder) http.HandlerFunc {
-	newPaths, err := urlshort.ToMap(decoder)
-	if err != nil {
-		fmt.Printf("Cannot decode file, skipping registration for this file")
+		fmt.Printf("Cannot decode file %s, skipping registration for this file\n", *path)
 		return redirector
 	}
 
 	return urlshort.Extend(redirector, newPaths)
 }
 
+/*
+	Default handler function which just writes Hello with the url path
+*/
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
+
 func main() {
+	// read in flags to get the file names of the JSON and YAML files which contain our routes
 	yamlPath := flag.String("YAML", "paths.yaml", "A YAML file")
 	jsonPath := flag.String("JSON", "paths.json", "A JSON file")
 	flag.Parse()
 
-	// Build the MapHandler using the mux as the fallback
-	pathsToUrls := map[string]string{
-		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
-	}
+	// default handler in case a path doesn't exist in either config
+	redirector := defaultHandler
 
-	newMux := http.NewServeMux()
-	redirector := hello
-	// register all the paths
-	redirector = urlshort.Extend(redirector, pathsToUrls)
+	// attempt to read in the file given via YAML flag
+	redirector = extendPaths(redirector, yamlPath, func(reader io.Reader) urlshort.Decoder {
+		decoder := yaml.NewDecoder(reader)
+		return decoder
+	})
 
-	yamlDecoder, err := getYAMLDecoder(yamlPath)
-	if err != nil {
-		fmt.Println("Cannot open yaml file, skipping registration for this file")
-	} else {
-		redirector = extendPaths(redirector, yamlDecoder)
-	}
+	// attempt to read in the file given via JSON flag
+	redirector = extendPaths(redirector, jsonPath, func(reader io.Reader) urlshort.Decoder {
+		decoder := json.NewDecoder(reader)
+		return decoder
+	})
 
-	jsonDecoder, err := getJSONDecoder(jsonPath)
-	if err != nil {
-		fmt.Println("Cannot open json file, skipping registration for this file")
-	} else {
-		redirector = extendPaths(redirector, jsonDecoder)
-	}
-
-	newMux.HandleFunc("/", redirector)
+	// set up handle func and run server
+	http.HandleFunc("/", redirector)
 	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", newMux); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalln("Unable to start server", err)
 	}
 }
--- urlshort/handler.go
+++ urlshort/handler.go
@@ -4,36 +4,52 @@ import (
 	"net/http"
 )
 
+// struct representing the URL path searched on our server and the location URL we want to redirect to
 type redirect struct {
 	Path string `yaml:"path" json:"path"`
 	URL  string `yaml:"url" json:"url"`
 }
 
-// Decoder Generic
+/*
+Decoder - Shared interface for Decoders. This allows us to write functions that work for all Decoder types instead
+of copying the same functions for json and yaml decoders.
+*/
 type Decoder interface {
 	Decode(v interface{}) (err error)
 }
 
-// Extend handler to have new endpoints
+/*
+Extend the startingHandler using the supplied paths. Requests with URL paths that are keys in the map will be redirected
+to the values for their respective keys. If the URL path is not in the map, we pass the request on to the next handler.
+*/
 func Extend(startingHandler http.HandlerFunc, paths map[string]string) http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
+		// if the url path is a key in the map
 		if paths[r.URL.Path] != "" {
+			// redirect to the value store for that key
 			http.Redirect(rw, r, paths[r.URL.Path], http.StatusFound)
 		}
 
+		// pass request on to the next handler
 		startingHandler.ServeHTTP(rw, r)
 	}
 }
 
-// ToMap with Decoder
-func ToMap(decoder Decoder) (map[string]string, error) {
+/*
+DecodeToMap - Given an incoming Decoder of any file type, decode it's contents into a map of the following format:
+	key - request url path (IE the url path the user will search)
+	value - redirect url (IE the url we will redirect to)
+*/
+func DecodeToMap(decoder Decoder) (map[string]string, error) {
 	var redirects []redirect
 
+	// attempt to decode into an array of redirects, return nil with error if an error is encountered
 	err := decoder.Decode(&redirects)
 	if err != nil {
 		return nil, err
 	}
 
+	// iterate through the redirects and insert them as key-value pairs in the map
 	paths := make(map[string]string)
 	for _, path := range redirects {
 		paths[path.Path] = path.URL
